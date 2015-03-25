272de6fab6ee8fe94bef5476196db174cf38b237
lampslave <lampslave@gmail.com>
2015-03-25 22:12:51 +0300

    initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hovel-annoying',
+    version='0.1',
+    description='',
+    long_description='',
+    url='https://github.com/hovel/hovel-annoying',
+    author='',
+    author_email='',
+    license='',
+    packages=['hovel_annoying'],
+)
