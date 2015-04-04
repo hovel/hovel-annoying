9d0d8606991f284768876c28b4b4076dc17d42fe
lampslave <lampslave@gmail.com>
2015-04-04 14:45:27 +0300

    json utils: add deserialize_jquery_form_data, improve js template

--- static_templates/json_form.js
+++ static_templates/json_form.js
@@ -11,6 +11,10 @@ $.ajax({
             //console.log(response.data.__FIELD__.label)
             //console.log(response.data.__FIELD__.errors[0].message)
             //console.log(response.data.__FIELD__.errors[0].code)
+
+            //$.each(response.data, function (field_name, field_data) {
+            //    $('__ERRORS__').append('<p>' + field_data.label + ': ' + field_data.errors[0].message + '</p>');
+            //});
         }
     },
     error: function (jqXHR, textStatus, errorThrown) {
