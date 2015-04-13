edb8ad364fa20b3390e085fe27a6d9bd19c38f93
lampslave <lampslave@gmail.com>
2015-04-13 22:24:42 +0300

    views: add mixin to communicate with contenteditable handler (initPlainContentEditable)

--- hovel_annoying/views.py
+++ hovel_annoying/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.core.exceptions import PermissionDenied
+from django.forms.models import modelform_factory
+from django.views import generic
+from django.shortcuts import get_object_or_404
+from hovel_annoying.json_utils import StatusJsonResponse
 
-# Create your views here.
+
+class PartialUpdateMixin(generic.View):
+    http_method_names = ['post']
+    model = None
+    fields = []
+
+    def post(self, request, pk):
+        obj = get_object_or_404(self.model, pk=pk)
+        field = request.POST.get('field')
+        if field not in self.fields:
+            raise PermissionDenied()
+        PartialUpdateForm = modelform_factory(self.model, fields=[field])
+        form = PartialUpdateForm(
+            data={field: request.POST.get('value')}, instance=obj
+        )
+        if form.is_valid():
+            form.save()
+            return StatusJsonResponse(True, {field: form.cleaned_data[field]})
+        else:
+            return StatusJsonResponse(False)
