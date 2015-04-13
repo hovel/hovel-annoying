1b546b6e52fff9ccd15a7517fbad5f48b26725c6
lampslave <lampslave@gmail.com>
2015-04-13 17:55:34 +0300

    ajax scripts: send query string instead of json, add contenteditable handler, update documentation

--- hovel_annoying/static/hovel_annoying/js/simple-ajax.js
+++ hovel_annoying/static/hovel_annoying/js/simple-ajax.js
@@ -1,20 +1,31 @@
 ;
-function SimpleAJAXRequest(url, data, onsuccess, $errors) {
-    /**
-     * @param {String} url
-     * @param data - data which ready to be encoded as a JSON string
-     * @param {Function} onsuccess - will be called after successful request, takes response.data as argument
-     * @param {Object} $errors - (jQuery) container for error messages
-     */
+/**
+ * @typedef {Object} jQuery
+ */
+
+/**
+ * Function to be called after successful request.
+ * @callback SimpleAJAXRequestCallback
+ * @param {Object} data - data returned from the server in response.data
+ */
+
+
+/**
+ * @param {String} url
+ * @param {(jQuery|String|Array)} data - data to send
+ * @param {SimpleAJAXRequestCallback} [onSuccess] - function to be called after successful request
+ * @param {jQuery} [$errors] - container for error messages
+ */
+function SimpleAJAXRequest(url, data, onSuccess, $errors) {
     $.ajax({
         url: url,
-        data: JSON.stringify(data),
+        data: data,
         dataType: 'json',
         method: 'POST',
         success: function (response, textStatus, jqXHR) {
             if (response.status == 'success') {
-                if (typeof onsuccess == 'function') {
-                    onsuccess(response.data);
+                if (typeof onSuccess == 'function') {
+                    onSuccess(response.data);
                 }
             } else if (response.status == 'fail') {
                 if (typeof $errors != 'undefined' && $errors.length) {
@@ -36,13 +47,14 @@ function SimpleAJAXRequest(url, data, onsuccess, $errors) {
     });
 }
 
-function initSimpleModalForm($modal, $form, onsuccess, $errors) {
-    /**
-     * @param {Object} $modal - (jQuery) modal window
-     * @param {Object} $form - (jQuery) form inside $modal
-     * @param {Function} onsuccess - will be called after successful request, takes response.data as argument
-     * @param {Object} $errors - (jQuery) container for error messages
-     */
+
+/**
+ * @param {jQuery} $modal - modal window
+ * @param {jQuery} $form - form inside $modal
+ * @param {SimpleAJAXRequestCallback} [onSuccess] - function to be called after successful request
+ * @param {jQuery} [$errors] - container for error messages
+ */
+function initSimpleModalForm($modal, $form, onSuccess, $errors) {
     $modal.on('hidden.bs.modal', function () {
         $form.trigger('reset');
         $errors.empty();
@@ -50,6 +62,40 @@ function initSimpleModalForm($modal, $form, onsuccess, $errors) {
     $form.on('submit', function (e) {
         e.preventDefault();
         $errors.empty();
-        SimpleAJAXRequest($form.attr('action'), $form.serializeArray(), onsuccess, $errors);
+        SimpleAJAXRequest($form.attr('action'), $form.serializeArray(), onSuccess, $errors);
+    });
+}
+
+
+/**
+ * @param {String} selector - CSS selector
+ * @param {String} field - name of the field to update
+ * @param {Function} urlBuilder - function that takes the edited jQuery element and returns url to request
+ * @param {Function} [onSuccess] - same as {@link SimpleAJAXRequestCallback}, but takes the edited jQuert element as second argument
+ */
+function initPlainContentEditable(selector, field, urlBuilder, onSuccess) {
+    $(selector).each(function () {
+        var $each = $(this);
+        $each.on('focus', function () {
+            var $focused = $(this);
+            $focused.data('original-' + field, $.trim($focused.text()));
+        });
+        $each.on('blur', function () {
+            var $blurred = $(this),
+                value = $.trim($blurred.text());
+            if (value == $blurred.data('original-' + field)) {
+                return;
+            }
+            SimpleAJAXRequest(
+                urlBuilder($blurred),
+                {'field': field, 'value': value},
+                function (data) {
+                    $blurred.text(data[field]);
+                    if (typeof onSuccess == 'function') {
+                        onSuccess(data, $blurred);
+                    }
+                }
+            );
+        });
     });
 }
