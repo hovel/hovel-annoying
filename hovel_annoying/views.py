1658a47e4b282040acb20a66b2f6c11eb38ca53f
lampslave <lampslave@gmail.com>
2015-04-30 10:09:04 +0300

    BasePartialUpdateView: add value filters

--- hovel_annoying/views.py
+++ hovel_annoying/views.py
@@ -17,11 +17,19 @@ class BasePartialUpdateView(SingleObjectMixin, View):
         if field not in self.fields:
             raise PermissionDenied()
         PartialUpdateForm = modelform_factory(self.model, fields=[field])
+        raw_value = self.filter_raw_value(request.POST.get('value'))
         form = PartialUpdateForm(
-            data={field: request.POST.get('value')}, instance=self.object
+            data={field: raw_value}, instance=self.object
         )
         if form.is_valid():
             form.save()
-            return StatusJsonResponse(True, {field: form.cleaned_data[field]})
+            saved_value = self.filter_saved_value(form.cleaned_data[field])
+            return StatusJsonResponse(True, {field: saved_value})
         else:
             return StatusJsonResponse(False)
+
+    def filter_raw_value(self, value):
+        return value
+
+    def filter_saved_value(self, value):
+        return value
