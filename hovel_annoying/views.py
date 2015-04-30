96e430ccc93e3a47d184d24e489c14f6e228a31c
lampslave <lampslave@gmail.com>
2015-04-30 11:00:39 +0300

    BasePartialUpdateView: store name of the edited field as instance property

--- hovel_annoying/views.py
+++ hovel_annoying/views.py
@@ -13,18 +13,20 @@ class BasePartialUpdateView(SingleObjectMixin, View):
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
-        field = request.POST.get('field')
-        if field not in self.fields:
+        self.field = request.POST.get('field')
+        if self.field not in self.fields:
             raise PermissionDenied()
-        PartialUpdateForm = modelform_factory(self.model, fields=[field])
+        PartialUpdateForm = modelform_factory(self.model, fields=[self.field])
         raw_value = self.filter_raw_value(request.POST.get('value'))
         form = PartialUpdateForm(
-            data={field: raw_value}, instance=self.object
+            data={self.field: raw_value}, instance=self.object
         )
         if form.is_valid():
             form.save()
-            saved_value = self.filter_saved_value(form.cleaned_data[field])
-            return StatusJsonResponse(True, {field: saved_value})
+            saved_value = self.filter_saved_value(
+                form.cleaned_data[self.field]
+            )
+            return StatusJsonResponse(True, {self.field: saved_value})
         else:
             return StatusJsonResponse(False)
 
